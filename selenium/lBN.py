b563d7b943131dbc3b783e51c7decc89282f43dc
Mark Schott <m.schott9@gmail.com>
2017-11-27 19:27:48 -0500

    mods

--- selenium/lBN.py
+++ selenium/lBN.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import csv
 
-#driver = webdriver.Chrome()
 driver = webdriver.Chrome()
 
 zzz=3
@@ -15,7 +14,7 @@ driver.get(url_of_interest)
 
 csv_file = open('results.csv', 'wb')
 writer = csv.writer(csv_file)
-writer.writerow(['title', 'subject'])
+#writer.writerow(['title', 'subject'])
 # Page index used to keep track of where we are.
 index = 1
 while True:
@@ -26,49 +25,49 @@ while True:
 
 		# Find all the results.
 		results = driver.find_elements_by_xpath('//*[@id="searchResultsArea"]/div/div')
-	#	results = driver.find_elements_by_xpath('//*[@id="contenu"]/div[2]/div[1]/div/div[1]/div[3]')
 #		print results
-		inner_index=1
 		for result in results:
-			print 'back again'
-
 			# open details area
 			button_path = './/*[@id="contenu"]/div[2]/div[1]/div/div[1]/div[3]/a'
 			button = result.find_element_by_xpath(button_path)
-			# Scroll into view
-#			driver.execute_script("return arguments[0].scrollIntoView(true);", button_path)
-		#	button = result.find_element_by_xpath('.//div[@class="main-infos"]//a[2]')
-#			print 'after button definition'
 			button.click()
-#			print 'after click'
 			time.sleep(3)
 			# Now the details area, with its info, should be open to scrape
+			
+			# Loop through key elements to get the value keys, there are also a variable number of elements
+			keys = driver.find_elements_by_xpath('.//*[@id="contenu"]/div[2]/div[1]/div/div[1]/div[4]/div/dl/dt')
+			keys_lst = []
+			for key in keys:
+				keys_lst.append(key.find_element_by_xpath('.//b').text.encode('utf-8'))
 
+			# Loop through key elements to get the value keys, there are also a variable number of elements
+                        values = driver.find_elements_by_xpath('.//*[@id="contenu"]/div[2]/div[1]/div/div[1]/div[4]/div/dl/dd')
+                        values_lst = []
+                        for value in values:
+                                values_lst.append(value.find_element_by_xpath('.').text.encode('utf-8'))
+		
+			writer.writerrow(values_lst)
+			#print keys_lst
+			#print values_lst
+			"""
 			result_dict = {}
 		#	title_xpath='.//*[@id="notice-details-1"]/div/dl/dd[1]'
 		#	subject_xpath='.//*[@id="notice-details-1"]/div/dl/dd[12]'
 			title_xpath='.//*[@id="contenu"]/div[2]/div[1]/div/div[1]/div[4]//dl/dd[1]'
 			subject_xpath='.//*[@id="contenu"]/div[2]/div[1]/div/div[1]/div[4]//dl/dd[12]'
-#			print 'after definitions'
 			title = result.find_element_by_xpath(title_xpath).text.encode('utf-8')
-#			print 'here 1'	
 			subject = result.find_element_by_xpath(subject_xpath).text.encode('utf-8')
-#			print 'here 2'
 			result_dict['title'] = title
 			result_dict['subject'] = subject
    			writer.writerow(result_dict.values())
 			print 'writing'
 			print title, subject
-			#script = "window.scrollTo(0, document.body.scrollHeight/%d);" % (50.0/inner_index)
-#			script = "window.scrollTo(0, document.body.scrollHeight);"
-#			driver.execute_script(script)
-#			inner_index = inner_index + 1
-
+			"""
 
 		# Locate the next button on the page.
 		button = driver.find_element_by_xpath('//*[@id="nextResultPage"]')
 		button.click()
-		index = 1                
+		#index = 1                
 
 	except Exception as e:
 		print e
